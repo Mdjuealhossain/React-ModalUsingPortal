3605d60ce7c1b502e265e4e5d389f8834d3ca651
Md_Jueal_Hossain <mdjueal920977@gmail.com>
2023-05-29 21:19:10 +0600

    Portalmodal

--- portal_modal/src/OtherComponents/ControModal.js
+++ portal_modal/src/OtherComponents/ControModal.js
@@ -8,9 +8,9 @@ const ControllModal=(props)=>{
  const    {togolmodal}=props
 
     return (
-        <div>
+        <>
 <button onClick={()=>togolmodal("eyhgtyt")}>Click to open Modal</button>
-        </div>
+        </>
     )
 }
 export default ControllModal
